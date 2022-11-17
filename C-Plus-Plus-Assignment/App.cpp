baa89b7fb2daa2763242e97da15a58c09b4b5606
Thomas Austin <austint@chc.edu>
2022-11-17 10:55:06 -0500

    Nov 17

---
+++ C-Plus-Plus-Assignment/App.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "./headers/Computation.h"
+#include "./headers/Statistician.h"
+
+using namespace std;
+
+int main() {
+    // create a Statistician object named s1 using its one-arg 
+    // constructor giving it an argument of 3.5
+    Statistician statistician1(3.5);
+
+    // declare local variables that will be used when calling 
+    // s1's getter method
+    Statistician lowest();
+    Statistician highest();
+    Statistician sum();
+
+    // call method on s1 that will display it to stdout
+    cout << "Statistitician 1 : ";
+    statistician1.printStatistician();
+
+    // call method on s1 that will add 5.2 to it
+    statistician1.addNumber(5.2);
+
+    // call method on s1 that will display it to stdout
+    statistician1.computeSum();
+    cout << "Statistician 1 : ";
+    statistician1.printStatistician();
+
+    // call method on s1 that will add -7.8 to it
+    statistician1.addNumber(-7.8);
+
+    // call method on s1 that will display it to stdout
+    cout << "Statistician 1 : ";
+    statistician1.printStatistician();
+
+    // call method on s1 that will get its field values
+    // and store them in local variables
+    statistician1.computeHighest();
+    statistician1.computeLowest();
+    statistician1.computeSum();
+
+    // display values in local variables to stdout
+    cout << "Statistician 1 Lowest : " << lowest << endl;
+    cout << "Statistician 1 Highest : " << highest << endl;
+    cout << "Statistician 1 Sum : " << sum << endl;
+
+    // create a Statistician object named s2 using its no-arg
+    // constructor
+    Statistician statistician2;
+
+    // call method on s2 that will display it to stdout
+    cout << "Statistician 2 : ";
+    statistician1.printStatistician();
+
+    // call method on s1 that will test if it is equal to s2
+    // and display its return to stdout as false or true
+    cout << "Statistician 1 equals Statistician 2 ? " <<
+        (statistician1.equalStatistician(statistician2) ? "true" : "false") << endl;
+
+    // call method on s2 that will add the double numbers to it 
+    // that will cause it to be equal to s1
+    statistician1.equalStatistician(statistician2);
+
+    // call method on s1 that will test if it is equal to s2
+    // and display its return to stdout as false or true
+    cout << "Statistician 1 equals Statistician 2 ? " <<
+        (statistician1.equalStatistician(statistician2) ? "true" : "false") << endl;
+}
---
+++ C-Plus-Plus-Assignment/headers/Computation.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+class Computation {
+    public:
+        virtual void computeLowest() = 0;
+        virtual void computeHighest() = 0;
+        virtual void computeSum() = 0;
+};
